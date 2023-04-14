ad4ca690431fbeff4185ead2b2f6c583703aa99f
Dmitriy <dfajl@mail.ru>
2023-04-14 15:09:10 +0300

    solved the second task pick

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,26 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  const res = {};
+  const arrayWithArgs = fields;
 
+  for (let i = 0; i < arrayWithArgs.length; i++) {
+    if (arrayWithArgs[i] in obj) {
+      res[arrayWithArgs[i]] = obj[arrayWithArgs[i]];
+    }
+  }
+  return res;
 };
+
+{
+  /* const object = {
+    age: 30,
+    name: "Dmitriy",
+    id: 4578,
+    isMarried: true,
+  };
+
+  function pick(obj, ...args) {}
+
+  console.log(pick(object, "age", "id", "name")); */
+}
