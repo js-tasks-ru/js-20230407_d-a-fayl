eee591fbd2ef28e637be15a315b9054aeb92e40a
Dmitriy <dfajl@mail.ru>
2023-04-18 18:17:41 +0300

    redid pick and omit one more time

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -1,16 +1,15 @@
 /**
- * omit - creates an object composed of enumerable property fields
+ * omit - creates an object composed of enumerable elemerty fields
  * @param {object} obj - the source object
- * @param {...string} fields - the properties paths to omit
+ * @param {...string} fields - the elemerties paths to omit
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
   const res = { ...obj };
-  const arrayWithArgs = fields;
 
-  for (let i = 0; i < arrayWithArgs.length; i++) {
-    if (arrayWithArgs[i] in res) {
-      delete res[arrayWithArgs[i]];
+  for (let elem of fields) {
+    if (elem in res) {
+      delete res[elem];
     }
   }
   return res;
